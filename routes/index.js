d13ada9a5e01e98d6d35e5fbedc34158226836ff
Elnur Kurtaliev <el92@ya.ru>
2016-04-23 23:03:30 +0300

    fix typo

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        title: 'DeviceDesk - Система контроля доступа к устройствам | Dasboard',
+        title: 'DeviceDesk - Система контроля доступа к устройствам | Dashboard',
         menu: 'index'
     });
 });
