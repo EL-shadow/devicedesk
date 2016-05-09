794b05f1d05845a788e0d659e60433248ca92af8
Elnur Kurtaliev <el92@ya.ru>
2016-05-09 18:50:09 +0300

    update admin page users

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('models/user').User;
 
 router.get('/', function (req, res, next) {
     res.render('admin/index', {
@@ -9,10 +10,25 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/users', function (req, res, next) {
-    res.render('admin/users', {
-        title: 'DeviceDesk - Пользователи',
-        menu: 'users'
-    });
+    User.find({ }, { firstname: 1, lastname: 1, email: 1 }, function(err, users) {
+        if (err) return next(err);
+        res.render('admin/users', {
+            title: 'DeviceDesk - Пользователи',
+            menu: 'users',
+            userList: users
+        });
+    })
+});
+
+router.delete('/user/:id', function (req, res, next) {
+    User.find({ }, { firstname: 1, lastname: 1, email: 1 }, function(err, users) {
+        if (err) return next(err);
+        res.render('admin/users', {
+            title: 'DeviceDesk - Пользователи',
+            menu: 'users',
+            userList: users
+        });
+    })
 });
 
 module.exports = router;
